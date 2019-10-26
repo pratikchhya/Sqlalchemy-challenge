bdb2a79757d2512d757530392e28dcee8f8f22cc
Pratikchhya <pratikchhyaa@gmail.com>
2019-10-26 15:21:38 -0400

    Updates

--- climate-app.py
+++ climate-app.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
-engine = create_engine("sqlite:///C:\Rutgers\Homework\Sqlalchemy-challenge\Resources\hawaii.sqlite")
+engine = create_engine("sqlite:///Resources\hawaii.sqlite")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -50,68 +50,55 @@ def hawaii():
     )
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-    """Return a list of all passenger names"""
-    # Query all 
+    session = Session(engine) 
     results = session.query(Measurement.date, Measurement.prcp).all()
     session.close()
     all_prcps = []
     for date, prcp in results:
         measurement_dict = {}
-        measurement_dict["date"] = date
-        measurement_dict["prcp"] = prcp
+        measurement_dict[date] = prcp
+        
         all_prcps.append(measurement_dict)
     return jsonify(all_prcps)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # Create our session (link) from Python to the DB
     session = Session(engine)
-    
-    # Query all passengers
-    results = session.query(Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+    results = session.query(Station.station).all()
     session.close()
-  
-    all_stations = []
-    for name, latitude, longitude, elevation in results:
-        station_dict = {}
-        station_dict["name"] = name
-        station_dict["latitude"] = latitude
-        station_dict["longitude"] = longitude
-        station_dict["elevation"] = elevation
-        all_stations.append(station_dict)
-    return jsonify(all_stations)
+    res=list(np.ravel(results))
+    return jsonify(res)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
- 
    session = Session(engine)
-   #Query all tobs
-   tobs_results = session.query(Measurement.station, Measurement.tobs).filter(Measurement.date.between('2016-08-01', '2017-08-01')).all()
+   tobs_results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date.between('2016-08-01', '2017-08-01')).all()
    tobs_list=[]
    for tobs in tobs_results:
        tobs_dict = {}
-       tobs_dict["station"] = tobs[0]
-       tobs_dict["tobs"] = float(tobs[1])
+       tobs_dict[tobs[0]] = float(tobs[1])
        tobs_list.append(tobs_dict)
    return jsonify(tobs_list)
+
 
 @app.route('/api/v1.0/<start>')
 def start(start):
     session = Session(engine)
     results=(session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs))
-    .filter(Measurement.date>=start_date).all())
+             .filter(Measurement.date>=start_date).all())
+    session.close()
     trvl=list(np.ravel(results))   
     return jsonify(trvl) 
 
 @app.route('/api/v1.0/<start>/<end>')
 def start_end(start, end):
-        session = Session(engine)
-        results1=(session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs))
+    session = Session(engine)
+    results1=(session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs))
        .filter(Measurement.date>=start_date).filter(Measurement.date<=end_date).all())
-        return jsonify(results1)
+    session.close()
+    trvls=list(np.ravel(results1))
+    return jsonify(trvls)
 
    
     
